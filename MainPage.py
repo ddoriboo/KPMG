556a7d3212ad339420e0bd0619ea6a6e22edfba5
ddoriboo <147412626+ddoriboo@users.noreply.github.com>
2024-08-08 14:41:04 +0900

    Update MainPage.py

--- MainPage.py
+++ MainPage.py
@@ -1,6 +1,4 @@
 import streamlit as st
-from PIL import Image
-import base64
 
 # 페이지 설정
 st.set_page_config(page_title="KPMG AI 센터", layout="wide", initial_sidebar_state="collapsed")
@@ -17,55 +15,30 @@ hide_st_style = """
 """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
-# 이미지를 base64로 인코딩
-def get_base64_of_bin_file(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-# 이미지 파일을 base64로 인코딩
-img_base64 = get_base64_of_bin_file('IMG_7287.png')
-
 # CSS 스타일 정의
-css = f"""
+css = """
 <style>
-    .stApp {{
-        background-color: #0A2F5A;  /* KPMG 파란색 배경 */
-    }}
-    .fullscreen-container {{
+    body {
+        margin: 0;
+        padding: 0;
+        background-color: #0A2F5A;
+    }
+    .fullscreen-container {
         position: fixed;
         top: 0;
         left: 0;
-        right: 0;
-        bottom: 0;
+        width: 100vw;
+        height: 100vh;
         display: flex;
         justify-content: center;
         align-items: center;
-        z-index: 9999;
-    }}
-    .content-wrapper {{
-        width: 100%;
-        height: 100%;
-        background-image: url("data:image/png;base64,{img_base64}");
-        background-size: contain;
-        background-position: center;
-        background-repeat: no-repeat;
-        cursor: pointer;
-    }}
-    @media (max-aspect-ratio: 16/9) {{
-        .content-wrapper {{
-            width: 100%;
-            height: auto;
-            aspect-ratio: 16 / 9;
-        }}
-    }}
-    @media (min-aspect-ratio: 16/9) {{
-        .content-wrapper {{
-            width: auto;
-            height: 100%;
-            aspect-ratio: 16 / 9;
-        }}
-    }}
+        overflow: hidden;
+    }
+    .content-wrapper {
+        max-width: 100%;
+        max-height: 100%;
+        object-fit: contain;
+    }
 </style>
 """
 
@@ -73,7 +46,7 @@ css = f"""
 html = f"""
 <div class="fullscreen-container">
     <a href="https://drive.google.com/file/d/1FcBc0E9W6pW97JaDMgE1HODSlqZFFmWF/view?usp=sharing" target="_blank">
-        <div class="content-wrapper"></div>
+        <img src="IMG_7287.png" alt="KPMG AI 센터" class="content-wrapper">
     </a>
 </div>
 """
